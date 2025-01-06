c7750272a5587bc1d3cfff9efdf7daaa998b7093
kyzinatra <danilsolom@mail.ru>
2025-01-06 23:29:53 +0300

    a

--- apps/chess/src/app/page.tsx
+++ apps/chess/src/app/page.tsx
@@ -1,10 +1,9 @@
+import { Board } from "@components/board/board.component";
+import { Moves } from "@components/moves/moves.component";
+import { Rooms } from "@components/rooms/rooms.component";
 import { Suspense } from "react";
 
 import css from "./page.module.css";
-
-import { Board } from "@/components/board/board.component";
-import { Moves } from "@/components/moves/moves.component";
-import { Rooms } from "@/components/rooms/rooms.component";
 
 export default async function Home() {
 	return (
