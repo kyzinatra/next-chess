cf3cffa7fa27b072a32256307dc869f544b4709a
kyzinatra <main@kyzinatra.ru>
2025-01-07 00:57:23 +0300

    final fix url

--- apps/chess/src/constants/rooms.ts
+++ apps/chess/src/constants/rooms.ts
@@ -1,2 +1,2 @@
-export const CLIENT_URL = "https://kyzinatra-next-chess-2fb9.twc1.net";
+export const CLIENT_URL = "https://kyzinatra-next-chess-3d72.twc1.net";
 export const ROOM_NAME = "chess_room";
