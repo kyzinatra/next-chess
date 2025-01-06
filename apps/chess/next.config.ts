090aed9c8b9f27550f13831679d41d929a24addb
kyzinatra <main@kyzinatra.ru>
2025-01-07 00:07:30 +0300

    tring to fix bug

--- apps/chess/next.config.ts
+++ apps/chess/next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	experimental: {
-		// typedRoutes: true,
-		// reactCompiler: true,
-	},
+	reactStrictMode: true,
+	pageExtensions: [
+		"page.tsx",
+		"page.ts",
+		// FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+		// Instead, use `not-found.ts` as a workaround
+		// "ts" is required to resolve `not-found.ts`
+		// https://github.com/vercel/next.js/issues/65447
+		"ts",
+	],
 };
 
 export default nextConfig;
